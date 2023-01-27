0dc9e3d86963d62d0771f8f7de4641e439b472d6
Guilherme José <gui_guifonseca@hotmail.com>
2023-01-27 11:38:18 -0300

    Fix value of Form.Control

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,6 +50,7 @@ const Home = () => {
                 onChange={evt => updateInputValue(evt)}
                 type="text"
                 placeholder="Search a book"
+                value={currentValue}
               />
             </Col>
             <Col xs={2}>
