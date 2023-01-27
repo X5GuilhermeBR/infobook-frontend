c2a6512b7024bde635135e6af65cecb0c2199aaf
Guilherme José <gui_guifonseca@hotmail.com>
2023-01-27 12:37:33 -0300

    Add toast alert

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
   ClearButton,
   ContainerButton,
@@ -7,12 +7,14 @@ import {
   Title,
   Wrapper,
 } from './styles'
-import { Button, Col, Form, Row } from 'react-bootstrap'
+import { Toast, Col, Form, Row } from 'react-bootstrap'
 import { getBook } from '../../services/request'
 
 const Home = () => {
-  const [currentOption, setCurrentOption] = useState('')
   const [currentValue, setCurrenValue] = useState('')
+  const [currentSearchBy, setCurrentSearchBy] = useState('Title')
+  const [dataResult, setDataResult] = useState('')
+  const [toastError, setToastError] = useState(false)
 
   const options = [
     'Title',
@@ -24,9 +26,22 @@ const Home = () => {
     'OCLC',
   ]
 
+  function onChangeOption(evt: React.ChangeEvent<HTMLSelectElement>) {
+    setCurrentSearchBy(evt.target.value)
+  }
+
+  function toggleToastError() {
+    setToastError(!toastError)
+  }
+
   function findBook() {
-    const result = getBook('intitle', currentValue)
-    console.log('response', result)
+    getBook(currentSearchBy, currentValue).then(response => {
+      setDataResult(response.data)
+      if (response.data.totalItems == 0) {
+        setToastError(true)
+      }
+      console.log('response', response.data)
+    })
   }
 
   function updateInputValue(
@@ -35,43 +50,61 @@ const Home = () => {
     setCurrenValue(evt.target.value)
   }
 
-  function deleteInputValue() {
+  function clearSearch() {
     setCurrenValue('')
+    setCurrentSearchBy('Title')
   }
 
   return (
-    <Wrapper>
-      <ContainerSearch>
-        <Title>infobook!📚</Title>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Control
-                onChange={evt => updateInputValue(evt)}
-                type="text"
-                placeholder="Search a book"
-                value={currentValue}
+    <>
+      <Wrapper>
+        <ContainerSearch>
+          <Toast show={toastError} onClose={toggleToastError}>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
               />
+              <strong className="me-auto">Error</strong>
+              <small>11 mins ago</small>
+            </Toast.Header>
+            <Toast.Body>Nenhum resultado encontrado</Toast.Body>
+          </Toast>
+          <Title>infobook!📚</Title>
+
+          <Form>
+            <Row>
+              <Col>
+                <Form.Control
+                  onChange={evt => updateInputValue(evt)}
+                  type="text"
+                  placeholder="Search a book"
+                  value={currentValue}
+                />
+              </Col>
+              <Col xs={2}>
+                <Form.Select
+                  as="select"
+                  onChange={evt => onChangeOption(evt)}
+                  value={currentSearchBy}
+                >
+                  {options.map(option => (
+                    <option value={option}>{option}</option>
+                  ))}
+                </Form.Select>
+              </Col>
+            </Row>
+          </Form>
+          <ContainerButton>
+            <Col>
+              <FindButton onClick={() => findBook()}>Find 🔎</FindButton>
+              <ClearButton onClick={() => clearSearch()}>Clear 💣</ClearButton>
             </Col>
-            <Col xs={2}>
-              <Form.Select>
-                {options.map(option => (
-                  <option value={option}>{option}</option>
-                ))}
-              </Form.Select>
-            </Col>
-          </Row>
-        </Form>
-        <ContainerButton>
-          <Col>
-            <FindButton onClick={() => findBook()}>Find 🔎</FindButton>
-            <ClearButton onClick={() => deleteInputValue()}>
-              Clear 💣
-            </ClearButton>
-          </Col>
-        </ContainerButton>
-      </ContainerSearch>
-    </Wrapper>
+          </ContainerButton>
+        </ContainerSearch>
+      </Wrapper>
+    </>
   )
 }
 
--- src/services/request.js
+++ src/services/request.js
@@ -3,9 +3,8 @@ import axios from 'axios'
 const tokeGoogleBook = window.envKeys.REACT_APP_TOKEN_GOOGLE_BOOK
 const baseURL = window.envKeys.REACT_APP_BASE_URL
 
-export const getBook = (typeSearch, valueSearch) => {
-  return axios
-    .get(`${baseURL}?q=${typeSearch}:${valueSearch}&key${tokeGoogleBook}`)
-    .then(res => console.log('current', res))
-    .catch(err => console.log(err))
+export function getBook(typeSearch, valueSearch) {
+  return axios.get(
+    `${baseURL}?q=${typeSearch}:${valueSearch}&key${tokeGoogleBook}`
+  )
 }
